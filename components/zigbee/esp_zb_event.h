904621356a63b2566f676e9668e47df50cb2c750
raul <raul11@freenet.de>
2026-06-04 15:13:06 +0200

    handle onreport data in zb_event

--- components/zigbee/esp_zb_event.h
+++ components/zigbee/esp_zb_event.h
@@ -123,7 +123,7 @@ class ZBEvent {
     }
     if (attribute.data.value != nullptr) {
       // Copy the attribute value to avoid dangling pointer issues
-      size_t value_size = this->get_attribute_value_size_(attribute.data.type, attribute.data.value);
+      size_t value_size = ezb_zcl_get_attr_value_size(attribute.data.type, attribute.data.value);
       if (value_size > 4) {
         this->event_.set_attr.attribute.data.value = malloc(value_size);
         if (this->event_.set_attr.attribute.data.value != nullptr) {
@@ -139,13 +139,14 @@ class ZBEvent {
   void init_report_attr_data(const ezb_zcl_cmd_report_attr_message_t *message) {
     this->event_.report_attr.dst_endpoint = message->in.header->dst_ep;
     this->event_.report_attr.cluster = message->in.header->cluster_id;
-    this->event_.report_attr.variables = *message->in.variables;
+    this->event_.report_attr.variables.attr_id = message->in.variables->attr_id;
+    this->event_.report_attr.variables.attr_type = message->in.variables->attr_type;
     this->event_.report_attr.src_address = message->in.header->src_addr;
     this->event_.report_attr.src_endpoint = message->in.header->src_ep;
     if (message->in.variables->attr_value != nullptr) {
       // Copy the attribute value to avoid dangling pointer issues
       size_t value_size =
-          this->get_attribute_value_size_(message->in.variables->attr_type, message->in.variables->attr_value);
+          ezb_zcl_get_attr_value_size(message->in.variables->attr_type, message->in.variables->attr_value);
       if (value_size > 4) {
         this->event_.report_attr.variables.attr_value = malloc(value_size);
         if (this->event_.report_attr.variables.attr_value != nullptr) {
@@ -154,6 +155,22 @@ class ZBEvent {
       } else {
         memcpy(this->event_.report_attr.inline_data, message->in.variables->attr_value, value_size);
         this->event_.report_attr.variables.attr_value = this->event_.report_attr.inline_data;
+      }
+    }
+    ezb_zcl_report_attr_variable_t *var = message->in.variables;
+    ezb_zcl_report_attr_variable_t *var_last = &(this->event_.report_attr.variables);
+    while (var->next != nullptr) {
+      var = var->next;
+      size_t value_size = ezb_zcl_get_attr_value_size(var->attr_type, var->attr_value);
+      var_last->next = new ezb_zcl_report_attr_variable_t();
+      if (var_last->next != nullptr) {
+        var_last->next->attr_id = var->attr_id;
+        var_last->next->attr_type = var->attr_type;
+        var_last->next->attr_value = malloc(value_size);
+        if (var_last->next->attr_value != nullptr) {
+          memcpy(var_last->next->attr_value, var->attr_value, value_size);
+        }
+        var_last = var_last->next;
       }
     }
   }
@@ -166,10 +183,9 @@ class ZBEvent {
     this->event_.read_attr_resp.variables.status = variables->status;
     this->event_.read_attr_resp.variables.attr_id = variables->attr_id;
     this->event_.read_attr_resp.variables.attr_type = variables->attr_type;
-    // this->event_.read_attr_resp.variables.attribute.data.size = variables.attribute.data.size;
     //  Note: variables is a pointer to a struct/list; deep copy needed
     if (variables->attr_value != nullptr) {
-      size_t value_size = this->get_attribute_value_size_(variables->attr_type, variables->attr_value);
+      size_t value_size = ezb_zcl_get_attr_value_size(variables->attr_type, variables->attr_value);
       if (value_size > 4) {
         this->event_.read_attr_resp.variables.attr_value = malloc(value_size);
         if (this->event_.read_attr_resp.variables.attr_value != nullptr) {
@@ -184,7 +200,7 @@ class ZBEvent {
     ezb_zcl_read_attr_rsp_variable_t *var_last = &(this->event_.read_attr_resp.variables);
     while (var->next != nullptr) {
       var = var->next;
-      size_t value_size = this->get_attribute_value_size_(var->attr_type, var->attr_value);
+      size_t value_size = ezb_zcl_get_attr_value_size(var->attr_type, var->attr_value);
       var_last->next = new ezb_zcl_read_attr_rsp_variable_t();
       if (var_last->next != nullptr) {
         var_last->next->attr_id = var->attr_id;
@@ -196,110 +212,6 @@ class ZBEvent {
         }
         var_last = var_last->next;
       }
-    }
-  }
-
-  size_t get_attribute_value_size_(uint8_t type, void *value) {
-    // Determine attribute value size based on attribute type (and size)
-    uint8_t len = 0;
-    switch (type) {
-      case EZB_ZCL_ATTR_TYPE_BOOL:
-        return sizeof(bool);
-      case EZB_ZCL_ATTR_TYPE_UINT8:
-        return sizeof(uint8_t);
-      case EZB_ZCL_ATTR_TYPE_UINT16:
-        return sizeof(uint16_t);
-      case EZB_ZCL_ATTR_TYPE_UINT24:
-        return 3;  // 24 bits = 3 bytes
-      case EZB_ZCL_ATTR_TYPE_UINT32:
-        return sizeof(uint32_t);
-      case EZB_ZCL_ATTR_TYPE_UINT40:
-        return 5;  // 40 bits = 5 bytes
-      case EZB_ZCL_ATTR_TYPE_UINT48:
-        return 6;  // 48 bits = 6 bytes
-      case EZB_ZCL_ATTR_TYPE_UINT56:
-        return 7;  // 56 bits = 7 bytes
-      case EZB_ZCL_ATTR_TYPE_UINT64:
-        return sizeof(uint64_t);
-      case EZB_ZCL_ATTR_TYPE_INT8:
-        return sizeof(int8_t);
-      case EZB_ZCL_ATTR_TYPE_INT16:
-        return sizeof(int16_t);
-      case EZB_ZCL_ATTR_TYPE_INT24:
-        return 3;  // 24 bits = 3 bytes
-      case EZB_ZCL_ATTR_TYPE_INT32:
-        return sizeof(int32_t);
-      case EZB_ZCL_ATTR_TYPE_INT40:
-        return 5;  // 40 bits = 5 bytes
-      case EZB_ZCL_ATTR_TYPE_INT48:
-        return 6;  // 48 bits = 6 bytes
-      case EZB_ZCL_ATTR_TYPE_INT56:
-        return 7;  // 56 bits = 7 bytes
-      case EZB_ZCL_ATTR_TYPE_INT64:
-        return sizeof(int64_t);
-      case EZB_ZCL_ATTR_TYPE_MAP8:
-        return sizeof(uint8_t);
-      case EZB_ZCL_ATTR_TYPE_MAP16:
-        return sizeof(uint16_t);
-      case EZB_ZCL_ATTR_TYPE_MAP24:
-        return 3;  // 24 bits = 3 bytes
-      case EZB_ZCL_ATTR_TYPE_MAP32:
-        return sizeof(uint32_t);
-      case EZB_ZCL_ATTR_TYPE_MAP40:
-        return 5;  // 40 bits = 5 bytes
-      case EZB_ZCL_ATTR_TYPE_MAP48:
-        return 6;  // 48 bits = 6 bytes
-      case EZB_ZCL_ATTR_TYPE_MAP56:
-        return 7;  // 56 bits = 7 bytes
-      case EZB_ZCL_ATTR_TYPE_MAP64:
-        return sizeof(uint64_t);
-      case EZB_ZCL_ATTR_TYPE_DATA8:
-        return sizeof(uint8_t);
-      case EZB_ZCL_ATTR_TYPE_DATA16:
-        return sizeof(uint16_t);
-      case EZB_ZCL_ATTR_TYPE_DATA24:
-        return 3;  // 24 bits = 3 bytes
-      case EZB_ZCL_ATTR_TYPE_DATA32:
-        return sizeof(uint32_t);
-      case EZB_ZCL_ATTR_TYPE_DATA40:
-        return 5;  // 40 bits = 5 bytes
-      case EZB_ZCL_ATTR_TYPE_DATA48:
-        return 6;  // 48 bits = 6 bytes
-      case EZB_ZCL_ATTR_TYPE_DATA56:
-        return 7;  // 56 bits = 7 bytes
-      case EZB_ZCL_ATTR_TYPE_DATA64:
-        return sizeof(uint64_t);
-      case EZB_ZCL_ATTR_TYPE_ENUM8:
-        return sizeof(uint8_t);
-      case EZB_ZCL_ATTR_TYPE_ENUM16:
-        return sizeof(uint16_t);
-      case EZB_ZCL_ATTR_TYPE_SEMI:
-        return sizeof(uint16_t);
-      case EZB_ZCL_ATTR_TYPE_SINGLE:
-        return sizeof(float);
-      case EZB_ZCL_ATTR_TYPE_DOUBLE:
-        return sizeof(double);
-      case EZB_ZCL_ATTR_TYPE_STRING:
-        len = *((uint8_t *) value);  // first byte is length
-        return len + 1;              // length byte + string
-      case EZB_ZCL_ATTR_TYPE_TOD:
-        return sizeof(uint32_t);
-      case EZB_ZCL_ATTR_TYPE_DATE:
-        return sizeof(uint32_t);
-      case EZB_ZCL_ATTR_TYPE_UTC:
-        return sizeof(uint32_t);
-      case EZB_ZCL_ATTR_TYPE_CLUSTER_ID:
-        return sizeof(uint16_t);
-      case EZB_ZCL_ATTR_TYPE_ATTRIBUTE_ID:
-        return sizeof(uint16_t);
-      case EZB_ZCL_ATTR_TYPE_BAC_OID:
-        return sizeof(uint32_t);
-      case EZB_ZCL_ATTR_TYPE_EUI64:
-        return sizeof(uint64_t);
-      case EZB_ZCL_ATTR_TYPE_KEY128:
-        return 16;  // 128 bits = 16 bytes
-      default:
-        return 0;  // Unknown type
     }
   }
 };
