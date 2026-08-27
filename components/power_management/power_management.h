b4f42d8b14eacb8de2f79a10004a31c4296da731
Villersfr2 <152911529+Villersfr2@users.noreply.github.com>
2026-08-27 15:23:56 +0200

    Add timed light sleep test state

--- components/power_management/power_management.h
+++ components/power_management/power_management.h
@@ -21,15 +21,20 @@ class PowerManagementComponent : public Component {
 
  protected:
   void configure_pm_();
+  void stop_test_and_report_();
 
   bool enable_light_sleep_{true};
   bool power_down_peripherals_{true};
   bool sleep_debug_{false};
   bool pm_configured_{false};
+  bool test_finished_{false};
   uint32_t start_delay_ms_{30000};
   uint32_t sleep_debug_interval_ms_{10000};
+  uint32_t test_duration_ms_{120000};
+  uint32_t pm_start_ms_{0};
   uint32_t last_debug_ms_{0};
   uint64_t last_awake_us_{0};
+  uint64_t test_start_us_{0};
   uint64_t last_sleep_total_us_{0};
   uint32_t last_sleep_entries_{0};
 };
