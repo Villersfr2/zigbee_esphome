130a64915ee72eb68b832f2d60296c7bc6c1c8b4
Villersfr2 <152911529+Villersfr2@users.noreply.github.com>
2026-08-27 16:06:25 +0200

    Simplify power management to native Zigbee automatic light sleep

--- components/power_management/power_management.h
+++ components/power_management/power_management.h
@@ -4,10 +4,6 @@
 
 #include "esphome/core/component.h"
 
-#ifdef CONFIG_PM_ENABLE
-#include "esp_timer.h"
-#endif
-
 namespace esphome {
 namespace power_management {
 
@@ -23,35 +19,15 @@ class PowerManagementComponent : public Component {
   void set_sleep_debug(bool enable) { this->sleep_debug_ = enable; }
   void set_sleep_debug_interval(uint32_t interval_ms) { this->sleep_debug_interval_ms_ = interval_ms; }
 
-  // Called by the native ESP-IDF one-shot timer after it wakes automatic light sleep.
-  void finish_sleep_test_from_timer();
-
  protected:
   void configure_pm_();
-  void stop_test_and_report_();
-  void print_stored_test_result_();
 
   bool enable_light_sleep_{true};
   bool power_down_peripherals_{true};
   bool sleep_debug_{false};
   bool pm_configured_{false};
-  bool test_finished_{false};
   uint32_t start_delay_ms_{30000};
   uint32_t sleep_debug_interval_ms_{10000};
-  uint32_t test_duration_ms_{120000};
-  uint32_t pm_start_ms_{0};
-  uint64_t test_start_us_{0};
-
-#ifdef CONFIG_PM_ENABLE
-  esp_timer_handle_t test_timer_{nullptr};
-#endif
-
-  uint64_t result_elapsed_us_{0};
-  uint64_t result_sleep_us_{0};
-  uint64_t result_awake_us_{0};
-  int64_t result_last_sleep_us_{0};
-  uint32_t result_entries_{0};
-  float result_ratio_{0.0f};
 };
 
 }  // namespace power_management
